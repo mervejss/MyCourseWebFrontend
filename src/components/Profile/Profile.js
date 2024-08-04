d7ca36d3d318a8ace96b523673b666034e9a82b6
aumervejss <72447846+mervejss@users.noreply.github.com>
2024-08-04 05:22:33 +0300

    "Updated Profile component CSS and JS files: added new CSS class, modified existing styles, and refactored JS code to include date formatting and updated user profile details display."

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,26 +10,45 @@ const Profile = () => {
   useEffect(() => {
     if (userID) {
       axios.get(`http://localhost:8080/users/${userID}/profile`)
-        .then(response => setUser(response.data))
+        .then(response => {
+          console.log(response.data); // Yanıtı kontrol edin
+          setUser(response.data);
+        })
         .catch(error => console.error('Error fetching user:', error));
     }
   }, [userID]);
+
+  // Tarih ve saati formatlama
+  const formatDate = (dateString) => {
+    const options = { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric',
+      hour: '2-digit', 
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false // 24 saat formatı için
+    };
+    return new Date(dateString).toLocaleString(undefined, options);
+  };
 
   if (!user) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="profile-container">
+    <div className="profile-page">
       <h1>Merhaba <strong>{user.userFullName}</strong>, Profil detaylarınızı buradan inceleyebilirsiniz</h1>
-      <div className="profile-details">
-        <p><strong>Kullanıcı Kimlik:</strong> {user.userID}</p>
-        <p><strong>Kullanıcı Tipi:</strong> {user.roleType}</p>
-        <p><strong>Kullanıcı Adı Soyadı:</strong> {user.userFullName}</p>
-        <p><strong>Kullanıcı Adı:</strong> {user.userName}</p>
-        <p><strong>Kullanıcı E-Posta:</strong> {user.userMail}</p>
-        <p><strong>Kullanıcı Şifre:</strong> ******</p>
-        <p><strong>Kullanıcı Kayıt Olma Tarihi:</strong> {new Date(user.createdAt).toLocaleDateString()}</p>
+      <div className="profile-container">
+        <div className="profile-details">
+          <p><strong>Kullanıcı Kimlik Numarası:</strong> {user.userID}</p>
+          <p><strong>Kullanıcı Tipi:</strong> {user.roleType}</p>
+          <p><strong>Kullanıcı Adı Soyadı:</strong> {user.userFullName}</p>
+          <p><strong>Kullanıcı Rumuz:</strong> {user.userName}</p>
+          <p><strong>Kullanıcı E-Posta:</strong> {user.userMail}</p>
+          <p><strong>Kullanıcı Şifre:</strong> {user.userPassword} </p>
+          <p><strong>Kullanıcı Kayıt Olma Tarihi:</strong> {user.createdAt ? formatDate(user.createdAt) : 'Bilgi Yok'}</p>
+        </div>
       </div>
     </div>
   );
