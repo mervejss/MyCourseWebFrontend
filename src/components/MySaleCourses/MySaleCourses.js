a907a75733cda353de45028a099287e875dc7177
aumervejss <72447846+mervejss@users.noreply.github.com>
2024-08-14 05:31:12 +0300

    "Added handleCancelOrder function to MySaleCourses component"

--- src/components/MySaleCourses/MySaleCourses.js
+++ src/components/MySaleCourses/MySaleCourses.js
@@ -113,6 +113,7 @@ const MySaleCourses = () => {
       console.error("Error updating transaction status", error);
     }
   };
+  
  const handleCancelOrder = async (transactionID) => {
     try {
       await axios.delete(`http://localhost:8080/transactions/${transactionID}`);
